788d5a0f189b120cfc93d59e6d1888c6a6eb9373
yuexex <134803066+yuexex@users.noreply.github.com>
2024-06-27 12:50:31 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(
-  base: "/magic-diagramatics-2/"
+  base: "/magic-diagramatics-2/",
   plugins: [react()],
 })
