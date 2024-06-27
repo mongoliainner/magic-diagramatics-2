af61b48213a623b8cf17a614539059f83140d368
yuexex <134803066+yuexex@users.noreply.github.com>
2024-06-27 12:48:29 +0300

    commit a suicide

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(
+  base: "/magic-diagramatics-2/"
   plugins: [react()],
 })
